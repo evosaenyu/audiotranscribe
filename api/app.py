5867b7e8cff6f23295613a461852f35ae9d61272
Chris <cso260@nyu.edu>
2024-05-19 02:06:42 -0400

    changed endpoint name to generate and new requirements

--- api/app.py
+++ api/app.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI, WebSocket
 from src.graph import AgentConstructor
 
-app = FastAPI() 
+app = FastAPI()
 
-
-@app.websocket("/ws")
+@app.websocket("/generate")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     agent = AgentConstructor(ws=websocket) 
     state = await agent.generate()
+    print(state["story"], "poop")
     await websocket.send_json({"status": 200, "generation": True,"response": state["story"]})
     await websocket.close(code=1000, reason="request fulfilled")
 
