0ea06041c9d4fecd532da09eb873ecfe4b4f585a
Parth Bhagat <parthb@nyu.edu>
2024-05-19 21:09:40 -0700

    fixing @evosae the asshat's sloppy code + upping revision limit (56604)

--- api/app.py
+++ api/app.py
@@ -7,8 +7,7 @@ app = FastAPI()
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     agent = AgentConstructor(ws=websocket) 
-    state = await agent.generate()
-    print(state["story"], "poop")
+    state = await agent.generate(revision_limit=10)
     await websocket.send_json({"status": 200, "generation": True,"response": state["story"]})
     await websocket.close(code=1000, reason="request fulfilled")
 
